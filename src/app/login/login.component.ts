048ef407f94b74975454abcc158447af6b84c6ec
MWorksCoding <mail.org@posteo.org>
2024-04-01 01:35:20 +0200

    login error message

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,6 +9,7 @@ import { AuthService } from '../auth.service';
 })
 export class LoginComponent {
   loading: boolean = false;
+  failedLogin: boolean = false;
   username: string = '';
   password: string = '';
   constructor(private router: Router, private auth: AuthService) {}
@@ -20,18 +21,19 @@ export class LoginComponent {
    * saving token to local storage
    */
   async login() {
-    this.showLoadingScreen()
+    this.failedLogin = false;
+    this.loading = true;
     try {
       let resp: any = await this.auth.loginWithUsernameAndPassword(
         this.username,
         this.password
       );
-      this.closeLoadingScreen()
+      this.loading = false;
       localStorage.setItem('token', resp['token']);
       this.router.navigateByUrl('/todos');
     } catch (e) {
-      this.closeLoadingScreen()
-      alert('Login failed');
+      this.loading = false;
+      this.failedLogin = true;
       console.error(e);
     }
   }
